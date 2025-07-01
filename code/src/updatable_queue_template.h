e02b250b39bbe4a6a8f278e7b04a39fdb9d15dea
Max <max.royer@ens-lyon.fr>
2025-07-01 15:24:40 +0200

    added some include

--- code/src/updatable_queue_template.h
+++ code/src/updatable_queue_template.h
@@ -1,21 +1,25 @@
 #pragma once
+#include <memory>
 #include <optional>
 
 template <typename Item, typename Priority, typename Key> class UpdatableQueue {
 public:
   virtual ~UpdatableQueue() = default;
 
-  virtual bool push(std::unique_ptr<Item> &&item_ptr, const Priority &priority) = 0;
+  virtual bool push(std::unique_ptr<Item> &&item_ptr,
+                    const Priority &priority) = 0;
 
   virtual std::optional<std::unique_ptr<Item>> pop() = 0;
 
   virtual bool remove(const Key &key) = 0;
 
-  virtual bool update(std::unique_ptr<Item> &&item_ptr, const Priority &newPriority) = 0;
+  virtual bool update(std::unique_ptr<Item> &&item_ptr,
+                      const Priority &newPriority) = 0;
 
-  virtual bool set(std::unique_ptr<Item> &&item_ptr, const Priority &newPriority) = 0;
+  virtual bool set(std::unique_ptr<Item> &&item_ptr,
+                   const Priority &newPriority) = 0;
 
   virtual bool empty() const = 0;
 
-  virtual size_t size() const  = 0;
+  virtual size_t size() const = 0;
 };
